b4bb7da38fc42b122c4f82a78e93be40c043a511
klayveR <tobiashoffmann7@gmail.com>
2021-08-11 18:08:08 +0200

    feat: secure local storage

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,22 @@
 import { createStore } from "vuex";
 import VuexPersistence from "vuex-persist";
+import SecureLS from "secure-ls";
+import simpleBrowserFingerprint from "simple-browser-fingerprint";
 
 import accounts from "@/store/modules/accounts";
 import hosts from "@/store/modules/hosts";
 import reports from "@/store/modules/reports";
 
+const ls = new SecureLS({
+    encodingType: "AES",
+    encryptionSecret: simpleBrowserFingerprint(),
+    isCompression: true,
+});
+
 const vuexLocal = new VuexPersistence({
-    storage: window.localStorage,
+    key: "vuex",
+    saveState: (key, state) => ls.set(key, state),
+    restoreState: (key) => ls.get(key),
 });
 
 export default createStore({
